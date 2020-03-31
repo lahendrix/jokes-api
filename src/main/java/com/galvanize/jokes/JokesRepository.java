40d7e3905125a38c59a2d6abc2335705e59972f2
Larry A. Hendrix <lahendrix@uwalumni.com>
2020-03-31 12:16:30 -0500

    Step 3: Fix the next error: cannot find symbol JokesRepository jokesRepository, by adding a JpaRepository. We do not need a test before writing the repository class because we are simply extending the JPA framework. There's no need to test the framework.

---
+++ src/main/java/com/galvanize/jokes/JokesRepository.java
@@ -0,0 +1,8 @@
+package com.galvanize.jokes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+interface JokesRepository extends JpaRepository<Joke, Long> {
+}
