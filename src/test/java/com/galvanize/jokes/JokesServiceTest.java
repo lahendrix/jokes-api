80735a6453a9a3132506f8426e3473dbcedecf34
Larry A. Hendrix <lahendrix@uwalumni.com>
2020-03-31 12:10:52 -0500

    Address first error: //error: cannot find symbol JokesService jokesService by writing a failing JokesServiceTest that will allow us to write that code.

---
+++ src/test/java/com/galvanize/jokes/JokesServiceTest.java
@@ -0,0 +1,38 @@
+package com.galvanize.jokes;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class JokesServiceTest {
+
+    @Autowired
+    JokesRepository jokesRepository;
+
+    JokesService jokesService;
+
+    @BeforeEach
+    void setup() {
+        jokesService = new JokesService(jokesRepository);
+    }
+
+    @Test
+    void createJoke_returnsJoke() {
+        // Setup
+        Joke expectedJoke = new Joke(JokeCategory.DADJOKES, "Yo dad so small, everybody thinks he's small.");
+
+        // Exercise
+        Joke savedJoke = jokesService.createjoke(expectedJoke);
+
+        // Assert
+        assertNotNull(savedJoke.getId());
+        assertEquals(expectedJoke.getCategory(), savedJoke.getCategory());
+        assertEquals(expectedJoke.getDescription(), savedJoke.getDescription());
+    }
+}
+
