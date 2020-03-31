87a54e21defded13e4fd5ad6f688dc60bb1d35b3
Larry A. Hendrix <lahendrix@uwalumni.com>
2020-03-31 11:42:20 -0500

    Initial commit.

---
+++ src/test/java/com/galvanize/jokes/JokesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvanize.jokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
