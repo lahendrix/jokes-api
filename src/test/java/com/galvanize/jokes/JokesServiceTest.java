0d9b7301c830ef8caf299498aa9a755cff25b8a6
Larry A. Hendrix <lahendrix@uwalumni.com>
2020-03-31 16:25:25 -0500

    Step-17: Write failing service test for getAllJokes to force writing the implementation.

--- src/test/java/com/galvanize/jokes/JokesServiceTest.java
+++ src/test/java/com/galvanize/jokes/JokesServiceTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -33,6 +36,24 @@ class JokesServiceTest {
         assertNotNull(savedJoke.getId());
         assertEquals(expectedJoke.getCategory(), savedJoke.getCategory());
         assertEquals(expectedJoke.getDescription(), savedJoke.getDescription());
+    }
+
+    @Test
+    void getAllJokes_returnsJokes() {
+        // Setup
+        Joke expectedJoke = new Joke(JokeCategory.KNOCKKNOCK, "A bad joke");
+        jokesRepository.save(expectedJoke);
+
+        // Exercise
+        List<Joke> actual = jokesService.getAllJokes();
+
+        // Assert
+        assertEquals(1, actual.size());
+        assertEquals(expectedJoke.getCategory(), actual.get(0).getCategory());
+        assertEquals(expectedJoke.getDescription(), actual.get(0).getDescription());
+
+        // Teardown
+        jokesRepository.deleteAll();
     }
 }
 
